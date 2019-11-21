76280c0df858ea3811be3305a13ac3318c14e446
paradajz <2544094+paradajz@users.noreply.github.com>
2019-11-21 10:28:22 +0100

    add second test

--- tests/test_demo.cpp
+++ tests/test_demo.cpp
@@ -11,3 +11,8 @@ TEST_CASE(demoTest)
 {
     TEST_ASSERT(offset.address(0) == offset.offset());
 }
+
+TEST_CASE(demoTest2)
+{
+    TEST_ASSERT(offset.address(0xfedcbaabcdef) == offset.offset() + 0xfedcbaabcdef);
+}
